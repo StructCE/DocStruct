669cad68bfa5d6885a3a838425844913185e30f9
paulo <paulofvilarimda@gmail.com>
2024-03-08 14:12:34 -0300

    ARIA para o menu sidebar

--- public/script.js
+++ public/script.js
@@ -1,8 +1,34 @@
+// declarações dos elementos a serem utilziados no script
 const toggleSideBarElements = document.querySelectorAll(".__toggle-sidebar"),
+  buttonsDocument = document.querySelectorAll(["a", "button"]),
   containerSideBar = document.querySelector(".container-sidebar"),
-  toggleTheme = document.querySelector(".__toggle-theme"),
-  body = document.querySelector("body");
+  toggleThemeButton = document.querySelector(".__toggle-theme"),
+  body = document.body;
 
+// permite o usuário usar o menu responsivo com sidebar
+function toggleContainerSideBar() {
+  containerSideBar.classList.toggle("show-container-sidebar");
+
+  if (containerSideBar.classList.contains("show-container-sidebar")) {
+    buttonsDocument.forEach((element) => {
+      if (!element.classList.contains("__button-sidebar")) {
+        element.setAttribute("tabindex", "-1");
+      }
+    });
+  } else {
+    buttonsDocument.forEach((element) => {
+      if (!element.classList.contains("__button-sidebar")) {
+        element.removeAttribute("tabindex");
+      }
+    });
+  }
+}
+
+toggleSideBarElements.forEach((element) => {
+  element.addEventListener("click", toggleContainerSideBar);
+});
+
+// seta o tema inicial do site para o usuário
 (function () {
   (m = localStorage.getItem("doc_theme")), (wm = window.matchMedia);
   if (
@@ -10,29 +36,21 @@ const toggleSideBarElements = document.querySelectorAll(".__toggle-sidebar"),
     (!m && wm && !wm("(prefers-color-scheme: dark)").matches)
   ) {
     body.classList.add("light");
-    toggleTheme.classList.add("light");
+    toggleThemeButton.classList.add("light");
   } else {
     body.classList.remove("light");
-    toggleTheme.classList.remove("light");
+    toggleThemeButton.classList.remove("light");
   }
 })();
 
-function toggleContainerSideBar() {
-  containerSideBar.classList.toggle("show-container-sidebar");
-}
+// permite o usuário alterar o tema
+toggleThemeButton.addEventListener("click", () => {
+  body.classList.toggle("light");
+  toggleThemeButton.classList.toggle("light");
 
-toggleSideBarElements.forEach((element) => {
-  element.addEventListener("click", toggleContainerSideBar);
-});
-
-toggleTheme.addEventListener("click", () => {
   if (body.classList.contains("light")) {
-    localStorage.setItem("doc_theme", "dark");
-    toggleTheme.classList.remove("light");
-    body.classList.remove("light");
-  } else {
     localStorage.setItem("doc_theme", "light");
-    toggleTheme.classList.add("light");
-    body.classList.add("light");
+  } else {
+    localStorage.setItem("doc_theme", "dark");
   }
 });
